c7220618a0bdf7f815d9be5fb858189ec1e66d16
logiclabstudio <mosamokgesi@gmail.com>
2026-02-19 12:43:55 +0200

    fix

---
+++ Vocentra.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vocentra.Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
